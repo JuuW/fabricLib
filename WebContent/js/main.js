4b975c1494a1af45398d4678eda7d88a95e02904
juuw <easythem@163.com>
2014-09-18 13:12:46 +0800

    and

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -3,42 +3,25 @@ $(document).ready(function() {
     console.log("Let's start :" + new Date().getMilliseconds());
 
     $("#nav-user").click(function() {
-        navUser()
+    	getNavPage("subpages/user.html");
     });
     $("#nav-fabric").click(function() {
-        navFabric()
+    	getNavPage("subpages/fabric.html");
     });
-    //$(".nav_item").mouseover(function(){navMouseover(this)});
 });
 
-
-function navUser() {
-    $.ajax({
-        type: "get",
-        url: "subpages/user.html",
-        cache: false,
-        success: function(data) {
-            $("#upper").html(data);
-        },
-        error: function() {
-            $("#upper").html("error");
-        }
-    });
+function getNavPage(pageUrl) {
+	$("#upper").html("");
+	$("#middle").html("");
+	$.ajax({
+		type : "get",
+		url : pageUrl,
+		cache : false,
+		success : function(data) {
+			$("#upper").html(data);
+		},
+		error : function() {
+			$("#upper").html("error");
+		}
+	});
 }
-
-function navFabric() {
-    $.ajax({
-        type: "get",
-        url: "subpages/fabric.html",
-        cache: false,
-        success: function(data) {
-            $("#upper").html(data);
-        },
-        error: function() {
-            $("#upper").html("error");
-        }
-    });
-}
-
-
-function navMouseover(obj) {}
