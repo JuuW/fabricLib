faee7bfb66094d0c441c8e6a241c9e8fcf77333a
anypossiblew <anypossible.w@gmail.com>
2014-09-20 22:59:04 +0800

    add css lib bootstrap
    add css lib bootstrap 3.2

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,27 +1,38 @@
-$(document).ready(function() {
+(function() {
+    $(document).ready(function() {
 
-    console.log("Let's start :" + new Date().getMilliseconds());
+        console.log("Let's start :" + new Date().getMilliseconds());
 
-    $("#nav-user").click(function() {
-    	getNavPage("subpages/user.html");
+        $("#nav-user").click(function(e) {
+            e.preventDefault();
+            getNavPage("subpages/user.html");
+            activeMenu("nav-user");
+        });
+        $("#nav-fabric").click(function(e) {
+            e.preventDefault();
+            getNavPage("subpages/fabric.html");
+            activeMenu("nav-fabric");
+        });
     });
-    $("#nav-fabric").click(function() {
-    	getNavPage("subpages/fabric.html");
-    });
-});
 
-function getNavPage(pageUrl) {
-	$("#upper").html("");
-	$("#middle").html("");
-	$.ajax({
-		type : "get",
-		url : pageUrl,
-		cache : false,
-		success : function(data) {
-			$("#upper").html(data);
-		},
-		error : function() {
-			$("#upper").html("error");
-		}
-	});
-}
+    function getNavPage(pageUrl) {
+        $("#upper").html("");
+        $("#middle").html("");
+        $.ajax({
+            type : "get",
+            url : pageUrl,
+            cache : false,
+            success : function(data) {
+                $("#upper").html(data);
+            },
+            error : function() {
+                $("#upper").html("error");
+            }
+        });
+    }
+
+    function activeMenu(menu) {
+        $(".main-menu li.active").removeClass("active");
+        $(".main-menu #" + menu).addClass("active");
+    }
+})();
